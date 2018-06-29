93d4953e7c87bbd53eca0fae00636131ceb7b4e9
ouranoshong <ouranoshong@outlook.com>
2018-06-29 17:40:32 +0800

    Add new examples

--- std_str/src/main.rs
+++ std_str/src/main.rs
@@ -1,3 +1,5 @@
+use std::str;
+
 fn main() {
     // println!("Hello, world!");
 
@@ -36,4 +38,66 @@ fn main() {
     println!("Alice says: {}", alice);
     println!("Bob says: {}", bob);
 
+
+    let byte_escape = "I'm writing \x52\x75\x73\x74!";
+    println!("What are you doing\x3F (\\x3F means ?) {}", byte_escape);
+
+    let unicode_codepoint = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+
+    println!("Unicode character {} (U+211D) is called {}",
+            unicode_codepoint, character_name );
+
+    let long_string = "String literals
+                    can span multiple lines.
+                    The linebreak and indentation here ->\
+                    <- can be escaped too!";
+
+    println!("{}", long_string);
+
+    let raw_str = r"Escapes don't work here: \x3F \u{211D}";
+    println!("{}", raw_str);
+
+    // If you need quotes in a raw string, add a pair of #s
+    let quotes = r#"And then I said: "There is no escape!""#;
+    println!("{}", quotes);
+
+    // If you need "# in your string, just use more #s in the delimiter.
+    // There is no limit for the number of #s you can use.
+    let longer_delimiter = r###"A string with "# in it. And even "##!"###;
+    println!("{}", longer_delimiter);
+
+    // Note that this is not actually a &str
+    let bytestring: &[u8; 20] = b"this is a bytestring";
+
+    // Byte arrays don't have Display so printing them is a bit limited
+    println!("A bytestring: {:?}", bytestring);
+
+    // Bytestrings can have byte escapes...
+    let escaped = b"\x52\x75\x73\x74 as bytes";
+    // ...but no unicode escapes
+    // let escaped = b"\u{211D} is not allowed";
+    println!("Some escaped bytes: {:?}", escaped);
+
+
+    // Raw bytestrings work just like raw strings
+    let raw_bytestring = br"\u{211D} is not escaped here";
+    println!("{:?}", raw_bytestring);
+
+    // Converting a byte array to str can fail
+    if let Ok(my_str) = str::from_utf8(raw_bytestring) {
+        println!("And the same as text: '{}'", my_str);
+    }
+
+    // let quotes = br#"You can also use "fancier" formatting, \
+    //                 like with normal raw strings"#;
+
+    // Bytestrings don't have to be UTF-8
+    let shift_jis = b"\x82\xe6\x82\xa8\x82\xb1\x82"; // "ようこそ" in SHIFT-JIS
+
+    // But then they can't always be converted to str
+    match str::from_utf8(shift_jis) {
+        Ok(my_str) => println!("Conversion successful: '{}'", my_str),
+        Err(e) => println!("Conversion failed: {:?}", e),
+    };
 }
